386205d8e85397bdbf1dbecb647b20ffbd86b83f
Drew <drewdavi@umich.edu>
2018-12-12 12:51:08 -0500

    add/delete activity in place. Handle illegal activity additions. Put add activity slides into app for intuitivity. Sort activities on gantt by EST.

---
+++ client/Schedule_App/AddActSlideTemplate.swift
@@ -0,0 +1,21 @@
+//
+//  AddActSlideTemplate.swift
+//  Schedule_App
+//
+//  Created by Drew Davis on 11/15/18.
+//  Copyright © 2018 Drew. All rights reserved.
+//
+
+import UIKit
+
+class AddActSlideTemplate: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
